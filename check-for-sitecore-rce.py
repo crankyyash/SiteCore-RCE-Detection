e40abb4992fa847e9076e899a94e55f739571868
crankyyash <61792333+crankyyash@users.noreply.github.com>
2022-09-22 18:02:37 +0530

    Add files via upload

---
+++ check-for-sitecore-rce.py
@@ -0,0 +1,52 @@
+import requests
+import re
+import xml.etree.ElementTree as ET
+from urllib3.exceptions import InsecureRequestWarning
+
+#Script to check sitecore version and whether its vulnerable to Pre-Auth RCE - CVE-2021-42237
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+url1 = "/sitecore/shell/ClientBin/Reporting/Report.ashx"
+url2 = "/sitecore/shell/sitecore.version.xml"
+f = open("urls1.txt")
+data = '<?xml version="1.0" ?><a></a>'
+lines = f.readlines()
+for i in lines:
+    try:
+        print(f"URL : {i.strip()}")
+        url3 = "https://" + i.strip() + url2;
+        print(f"[+] Checking sitecore version at {url3} :")
+        x = requests.get(url3, verify = False)
+        if x.status_code == 200 and re.findall("Sitecore Corporation",x.text):
+            root = ET.fromstring(x.content)
+            title = root[2].text
+            date = root[1].text
+            #print(title)
+            version = root[0][0].text + "." + root[0][1].text + "." + root[0][3].text
+            #print(version)
+            #print("\r\n")
+            print(f"Sitecore version : {title} {version} dated {date}")
+            url4 = "https://" + i.strip() + url1
+            print(f"[+]making get request to {url4} :")
+            r1 = requests.get(url4)
+            print("[+]status code:")
+            print(r1.status_code)
+            if r1.status_code == 200:
+                print(f"[+] Possible SiteCore RCE here! Visit {url4} to confirm")
+            print(f"[+]making post request to {url4} :")
+            r2 = requests.post(url4,data)
+            print(f"{r2.status_code}\r\n")
+            if r2.status_code == 200:
+                print(f"[+] Possible SiteCore RCE here! Visit {url4} to confirm")
+        elif x.status_code == 301:
+            print(f"[-] Redirected! No Sitecore here at {i.strip()}\r\n")
+        else:
+            print(f"[-] No Sitecore here at {i.strip()}\r\n")
+    except requests.exceptions.ConnectionError:
+        print("[-]Some connection error. Moving on.\r\n")
+        continue
+    except requests.exceptions.Timeout:
+        print("[-]Some timeout error. Moving on.\r\n")
+        continue
+
+f.close()
